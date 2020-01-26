b5507f559cec7c49b9aec45d09e4abcd4fddf92d
lloydnoone <lloydnoone92@gmail.com>
2020-01-26 19:15:40 +0000

    unit and integration tests

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default () => {
-  return (
-    <div>
-      Comment List
-    </div>
-  )
+class CommentList extends Component {
+  renderComments() {
+    return this.props.comments.map(comment => {
+      return <li key={comment}>{comment}</li>
+    })
+  }
+  
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.renderComments()}
+        </ul>
+      </div>
+    )
+  }
 }
+//get comments from the state in redux redux
+function mapStateToProps(state) {
+  return { comments: state.comments }
+}
+// connect passes redux state in the helper function, uses it map comments to props
+export default connect(mapStateToProps)(CommentList)
